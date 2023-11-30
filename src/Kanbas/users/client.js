589499f35ca3755fc8c8599a944719e2220e14cf
[elizabeth-jamison] <jamison.e@northeastern.edu>
2023-11-30 12:52:09 -0500

    client changed

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -8,7 +8,7 @@ export const BASE_API = process.env.REACT_APP_API_BASE;
 export const USERS_API = `${BASE_API}/users`;
 
 export const signin = async (credentials) => {
-  console.log("credentials: " + credentials);
+  console.log("credentials: " + JSON.stringify(credentials));
   const response = await request.post(`${USERS_API}/signin`, credentials);
   console.log("response: " + response);
   return response.data; };
