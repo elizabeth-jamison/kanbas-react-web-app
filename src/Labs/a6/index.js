1bf0ef15a31663b63d0598fbacca05c141a5addb
[elizabeth-jamison] <jamison.e@northeastern.edu>
2023-12-04 03:15:58 -0500

    a6

--- src/Labs/a6/index.js
+++ src/Labs/a6/index.js
@@ -1,8 +1,11 @@
 function Assignment6() {
-      return (
-        <div>
-          <h1>Assignment 6</h1>
-        </div>
-      );
-    }
-    export default Assignment6;
+  return (
+    <div>
+      <h1>Assignment 6</h1>
+      <ul>
+        <li>Implementation for Assignment 6 is on Kanbas page under Account.</li>
+      </ul>
+    </div>
+  );
+}
+export default Assignment6;
